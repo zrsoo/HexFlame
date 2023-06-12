be377415b5510c0f00d0f5b2c3d0318fb7118a35
zrsoo <46485193+zrsoo@users.noreply.github.com>
2023-06-13 01:37:11 +0300

    advanced billboard

---
+++ Assets/Flame/Scripts/AdancedBillboard.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class AdvancedBillboard : MonoBehaviour
+{
+    private Camera mainCamera;
+    private Vector3 initialPosition;
+    public float offsetRadius;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        initialPosition = transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 directionToCamera = mainCamera.transform.position - initialPosition;
+        directionToCamera.y = 0;  // This line keeps the flame's normal parallel to the ground.
+
+        // Normalize the direction to get a unit vector, and scale by the offset radius.
+        Vector3 offsetDirection = directionToCamera.normalized * offsetRadius;
+
+        // Set the new position to be the initial position plus the offset.
+        transform.position = initialPosition + offsetDirection;
+
+        if (directionToCamera != Vector3.zero) // To avoid errors when the object is exactly at the camera position
+        {
+            transform.rotation = Quaternion.LookRotation(-directionToCamera);
+        }
+    }
+}
